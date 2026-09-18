5bbc1695772994de1c4c75a87b35472ecbd86a4e
Krish Ramena <krish.ramena24@vit.edu>
2026-09-18 16:20:40 +0530

    Update Trade.jsx

--- src/Features/Trade.jsx
+++ src/Features/Trade.jsx
@@ -141,7 +141,7 @@ function Trade() {
   };
 
   return (
-    <div className="relative min-h-screen spg overflow-hidden bg-gradient-to-br from-slate-50 via-slate-100 to-slate-200 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 text-gray-900 dark:text-gray-100 font-sans transition duration-500">
+    <div className="relative min-h-screen spg  bg-gradient-to-br from-slate-50 via-slate-100 to-slate-200 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 text-gray-900 dark:text-gray-100 font-sans transition duration-500">
       <div
         className="pointer-events-none fixed top-0 left-0 w-full h-full z-0"
         style={{
